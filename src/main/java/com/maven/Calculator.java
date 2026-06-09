2f4af9a496528180055179977ee4b8c11cd484a1
nyompa <tshering.namgyal.dorji@gmail.com>
2026-06-10 03:47:38 +0600

    Readme.md

--- src/main/java/com/maven/Calculator.java
+++ src/main/java/com/maven/Calculator.java
@@ -8,4 +8,8 @@ public class Calculator {
     {
         return a/b;
     }
+    public int add(int a, int b){return a+b;}
+    public int sub(int a, int b){return a-b;}
+    public int mul(int a, int b){return a*b;}
+
 }
