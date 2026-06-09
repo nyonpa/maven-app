7b5227e975bafd82ef8483ce6418748fd42c17eb
nyompa <tshering.namgyal.dorji@gmail.com>
2026-06-10 03:38:42 +0600

    new functions

---
+++ src/main/java/com/maven/Calculator.java
@@ -0,0 +1,11 @@
+package com.maven;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Calculator {
+
+    public int div(int a , int b)
+    {
+        return a/b;
+    }
+}
--- src/test/java/com/test/test.java
+++ src/test/java/com/test/test.java
@@ -1,4 +1,18 @@
 package com.test;
+import com.maven.Calculator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class test {
+
+    @Test
+    void testCal()
+    {
+        Calculator c=new Calculator();
+        int actualResult = c.div(10,5);
+        int expectedResult = 2;
+        assertEquals(expectedResult, actualResult);
+
+    }
 }
